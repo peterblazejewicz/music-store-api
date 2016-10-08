3ffff7965e0755608bd05bd069919685d502ab6d
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-10-08 23:19:28 +0200

    Migrate implemetation sources
    - namesapce changes
    - changed structure: Controllers/Models/Infrastructure
    - fixed imports from NuGet
    - updates to package versions

---
+++ src/MusicStore.Api/Models/Artist.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicStore.Api.Models
+{
+    public class Artist
+    {
+        public int ArtistId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
--- src/MusicStore.Api/Program.cs
+++ src/MusicStore.Api/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 
-namespace MusicStoreApi
+namespace MusicStore.Api
 {
     public class Program
     {
